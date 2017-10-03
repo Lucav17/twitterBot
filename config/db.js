58fb1e8f86cdaa1abe9ed793a3081bc41a57d76e
Gianluca Vaccarini <gianlucavaccarini@gmail.com>
2017-10-02 20:11:33 -0700

    added back config for heroku

---
+++ config/db.js
@@ -0,0 +1,3 @@
+module.exports = {
+    DATABASE: "mongodb://root:root123@ds155634.mlab.com:55634/sudzlytweets"
+}
---
+++ config/twitterAPI.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key: 'zFfouAERYMg6eTMrHLteHopFu',
+    consumer_secret: 'BkYa96WEZGRFp35k7EzTqci6Xx3JGC0QQiESEK3ZJSBkqWFSHO',
+    access_token_key: '913459446692454400-plYG8vifUPNMC4PEoJIIZDPN5AxWRc9',
+    access_token_secret: 'B0XezGNVRkAAoCedzxhLLUS27uaudRoPMSOWEPW0MWOiF'
+}
